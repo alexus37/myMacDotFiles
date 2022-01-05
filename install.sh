89da15cdc98362007409cc6938e2ce82bf85ed0e
Alex Lelidis <alexus37@github.com>
2022-01-05 10:54:04 +0100

    make executuable v2

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 # install zsh plugins
-/bin/bash ./.scripts/install_zsh.sh
+git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+
+$HOME/.fzf/install --bin
+
+if [[ "$(command -v pip3)" ]]; then     
+    pip3 install --user thefuck; 
+fi 
